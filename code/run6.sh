26d4f1762eb5358f8f41fd073447d53ac4f7f490
Torbjörn Rathsman <milasudril@gmail.com>
2016-09-13 14:38:32 +0200

    Fixed bad help text

---
+++ code/run6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir_out=/home/torbjorr/temp2
+k=22000000
+l=1000
+m=20000
+
+mkdir -p $dir_out
+
+rm -rf $dir_out/$k-$l-$m
+mkdir $dir_out/$k-$l-$m
+
+__targets_rel/snowflake_simulate3 \
+	--shape=../crystal-prototypes/bullet.ice --deformation=L,1,0 \
+	--deformation=a,0.33 --deformation=t,0 \
+	--output-dir=$dir_out/$k-$l-$m --dump-stats=256 --N=4999 \
+	--growthrate=$k --meltrate=$l --droprate=$m \
+	--seed=0 --dump-geometry \
+	--stop-cond=infinity \
+	| tee $dir_out/$k-$l-$m/params.txt
