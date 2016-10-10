60f408759f37ab8e6ace7cb84c67f0da827365d5
Torbjörn Rathsman <milasudril@gmail.com>
2016-10-10 09:59:14 +0200

    Added shoot method. Does *not* use the same interface as volume_convex

--- code/sphere.h
+++ code/sphere.h
@@ -32,13 +32,41 @@ namespace SnowflakeModel
 				return 4.0f*std::acos(-1.0f)*r*r;
 				}
 
-			std::pair<Triangle,float> shoot(const Point& source
+			float shoot(const Point& source
 				,const Vector& direction
-				,float E_0,float decay_distance
-				,bool backface_culling) const noexcept;	
+				,float E_0,float decay_distance) const noexcept
+				{
+				Vector ret(INFINITY,INFINITY,INFINIT);
+				auto d=Vector(source-m_location);
+				auto a=glm::dot(direction,d);
+				auto b=glm::dot(d,d);
+				auto r2=m_radius;
+				r2*=r2;
+				
+				auto sqr=a*a-b+r2;
+				if(sqr<0.0f)
+					{return ret;}
+
+				sqr=std::sqrt(sqr);
+				auto distance=std::min(-a+sqr, -a-sqr);
+				assert(distance>=0.0f);
+
+				auto E_out=E_0*(1.0f - proj)
+					*std::exp(-temp.second/decay_distance);
+
+				return E_out<1.0f?distance:INFINITY;
+				}
 
 			float radiusGet() const noexcept
 				{return m_radius;}
+
+			BoundingBox boundingBoxGet() const noexcept
+				{
+				auto r=m_radius;
+				auto v=Point(r,r,r,0.0f);
+				auto pos=m_location;
+				return {terpos - v,pos + v};
+				};
 
 		private:
 			float m_radius;
