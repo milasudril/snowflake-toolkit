334875a318c7e684fd5793d261ffe4b2fd701854
Torbjörn Rathsman <milasudril@gmail.com>
2016-10-15 09:47:38 +0200

    Successfully some points

--- code/addaview.cpp
+++ code/addaview.cpp
@@ -8,7 +8,9 @@
 #include <GL/glew.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
-#include <glm/vec3.hpp>
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <memory>
@@ -62,7 +64,7 @@ static void close(GLFWwindow* handle)
 	}
 
 
-static const char* shader_vertex_src="#version 330\n"
+static const char* g_vertex_shader_src="#version 330\n"
 	"layout(location = 0) in vec3 vertex_pos_modelspace;\n"
 	"uniform mat4 MVP;\n"
 	"void main()\n"
@@ -70,7 +72,7 @@ static const char* shader_vertex_src="#version 330\n"
 	"	gl_Position=MVP*vec4(vertex_pos_modelspace,1);\n"
 	"	}\n";
 
-static const char* shader_fragment_src="#version 330\n"
+static const char* g_fragment_shader_src="#version 330\n"
 	"uniform vec3 fragment_color;\n"
 	"out vec3 color;\n"
 	"void main()\n"
@@ -128,10 +130,18 @@ static GLuint createShaderProgram(const char* vertex_shader_src
 	,const char* fragment_shader_src)
 	{
 	GlShader vertex_shader(GL_VERTEX_SHADER);
+	vertex_shader.sourceSet(vertex_shader_src);
+	vertex_shader.compile();
 	GlShader fragment_shader(GL_FRAGMENT_SHADER);
+	fragment_shader.sourceSet(fragment_shader_src);
+	fragment_shader.compile();
 	auto ret=glCreateProgram();
 	glAttachShader(ret,vertex_shader.idGet());
 	glAttachShader(ret,fragment_shader.idGet());
+	glLinkProgram(ret);
+	glDetachShader(ret, vertex_shader.idGet());
+	glDetachShader(ret, fragment_shader.idGet());
+	return ret;
 	}
 
 
@@ -144,6 +154,9 @@ class PointCloud
 	private:
 		GLuint vertex_array;
 		GLuint vbo;
+		GLuint shader;
+		GLuint mvp_location;
+		GLuint fragment_color_location;
 		
 		size_t n;
 	};
@@ -152,10 +165,12 @@ PointCloud::PointCloud(const std::vector<glm::vec3>& points):n(points.size())
 	{
 	glGenVertexArrays(1,&vertex_array);
 	glBindVertexArray(vertex_array);
-
-//	TODO: Compile shaders...
-
+	shader=createShaderProgram(g_vertex_shader_src,g_fragment_shader_src);
+	glUseProgram(shader);
+	mvp_location=glGetUniformLocation(shader,"MVP");
+	fragment_color_location=glGetUniformLocation(shader,"fragment_color");
 	glEnable(GL_DEPTH_TEST);
+	glPointSize(2);
 	glGenBuffers(1,&vbo);
 	glBindBuffer(GL_ARRAY_BUFFER,vbo);
 	glBufferData(GL_ARRAY_BUFFER,n*sizeof(glm::vec3),points.data(),GL_STATIC_DRAW);
@@ -163,13 +178,21 @@ PointCloud::PointCloud(const std::vector<glm::vec3>& points):n(points.size())
 
 void PointCloud::render() const noexcept
 	{
-	glPointSize(1);
+	glm::mat4 mvp;
+	glUniformMatrix4fv(mvp_location, 1, GL_FALSE, &mvp[0][0]);
+	glUniform3f(fragment_color_location,1,1,1);	
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
 	glDrawArrays(GL_POINTS,0,n);
+	glDisableVertexAttribArray(0);
 	}
 
 
 static void render(GLFWwindow* handle)
 	{
+	glClearColor(0,0,0,1);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	auto pc=reinterpret_cast<const PointCloud*>(glfwGetWindowUserPointer(handle));
 	pc->render();
@@ -179,10 +202,17 @@ static void render(GLFWwindow* handle)
 static std::vector<glm::vec3> pointsLoad()
 	{
 	SnowflakeModel::RandomGenerator rng;
-	std::uniform_real_distribution<float> U(-1,-1);
+	std::uniform_real_distribution<float> U(-1,1);
 	std::vector<glm::vec3> ret;
 	for(size_t k=0;k<1000;++k)
 		{ret.push_back({U(rng),U(rng),U(rng)});}
+
+/*	std::vector<glm::vec3> ret;
+	ret.push_back({-.5f, -.5f, 0.5f});
+	ret.push_back({.5f, -.5f, 0.5f});
+	ret.push_back({0.0f,  .5f, 0.5f});
+	ret.push_back({0.0f,0.0f,0.0f});*/
+
 	return std::move(ret);
 	}
 
