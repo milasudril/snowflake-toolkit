bc79e0891e9c39dc6c066491b1f419da640d7b78
Torbjörn Rathsman <milasudril@gmail.com>
2016-11-03 13:17:08 +0100

    Reduce complexity from n^2to n, by slicing input data

--- code/ice_load.m
+++ code/ice_load.m
@@ -7,16 +7,16 @@ function vols=ice_load(source,exepath)
 	system_wrapper({cmd,['--dump-geometry-simple=',f],['--prototype=',source]},0);
 	data=dlmread(f,' ');
 	vol_current=0;
-	while 1
-		ind=find(data(:,1)==vol_current);
-		if isempty(ind)
-			return;
-		end
-		v_rows=find(data(:,2)==0 & data(:,1)==vol_current);
-		v_faces=find(data(:,2)==1 & data(:,1)==vol_current);
-		vol_current=vol_current+1;
-		vols{vol_current}=struct('vertices',data(v_rows,3:5)...
-			,'faces',1 + data(v_faces,3:5));
+	slice=1;
+	sections=[0;diff(data(:,1))];
+	delims=[find(sections),size(data,1)];
+	for vol_current=1:numel(delims)
+		slice_start=slice;
+		slice=delims(vol_current)-1;
+		v_rows=find(data(slice_start:slice,2)==0);
+		v_faces=find(data(slice_start:slice,2)==1);
+		vols{vol_current}=struct('vertices',data(v_rows+slice_start - 1,3:5)...
+			,'faces',1 + data(v_faces+slice_start - 1,3:5));
 	end
 	delete(f);
 end
